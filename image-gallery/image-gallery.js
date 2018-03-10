166eab011218ee753e3338416648e5ec949c8de6
Kinstelle <christelle.mouele@gmail.com>
2018-03-10 18:29:04 +0100

    practical work: image gallery - javascript

---
+++ image-gallery/image-gallery.js
@@ -0,0 +1,33 @@
+// variables
+var overlay = document.querySelector('.overlay');
+var overlayClose = document.querySelector('.close');
+var overlayImage = document.querySelector('img');
+var gallery = document.querySelector('.gallery');
+
+var i, j;
+var imageNb = 50;
+var digits = [];
+
+// gallery items structure
+function generateHTML([col,row]) {
+    return `<div class="item col${col} row${row}"><img src="images/${randomNumber(12)}.jpg" alt=""><div class="item__overlay"><button>View</button></div></div>`;
+}
+
+// generate random number to chose the image
+function randomNumber(limit) {
+    return Math.floor(Math.random() * limit) + 1;
+}
+
+// generate arrays with col and row values
+for(i = 0; i < imageNb; i++) {
+    digits.push([randomNumber(4),randomNumber(4)]);
+}
+
+// generate arrays with 1 col and 1 row to fill blank spots
+for(j = 0; j < (imageNb); j++) {
+    digits.push([1,1]);
+}
+
+// generate gallery items (col, row, image) and put them in the html structure
+const html = digits.map(generateHTML).join('');
+gallery.innerHTML = html;
